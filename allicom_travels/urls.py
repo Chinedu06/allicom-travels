49ee2e95574117a17c4bbf6aff21099b12ba14e0
Okereke Godswill <godswillchinedu644@gmail.com>
2025-12-03 06:30:33 +0100

    Your message describing the change

--- allicom_travels/urls.py
+++ allicom_travels/urls.py
@@ -1,30 +1,54 @@
-"""
-URL configuration for allicom_travels project.
+# """
+# URL configuration for allicom_travels project.
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# The `urlpatterns` list routes URLs to views. For more information please see:
+#     https://docs.djangoproject.com/en/5.2/topics/http/urls/
+# Examples:
+# Function views
+#     1. Add an import:  from my_app import views
+#     2. Add a URL to urlpatterns:  path('', views.home, name='home')
+# Class-based views
+#     1. Add an import:  from other_app.views import Home
+#     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+# Including another URLconf
+#     1. Import the include() function: from django.urls import include, path
+#     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+# """
+# # from django.contrib import admin
+# # from django.urls import path, include
+
+
+# # urlpatterns = [
+# #     path('admin/', admin.site.urls),
+# #     path('users/', include('users.urls')),
+# #     path('api/', include('services.urls')),
+# #     path('api/', include('bookings.urls')),   # bookings router e.g. /api/bookings/
+# #     path('api/payments/', include('payments.urls')),
+# # ]
+
 # from django.contrib import admin
 # from django.urls import path, include
-
+# from django.conf import settings
+# from django.conf.urls.static import static
 
 # urlpatterns = [
 #     path('admin/', admin.site.urls),
-#     path('users/', include('users.urls')),
-#     path('api/', include('services.urls')),
-#     path('api/', include('bookings.urls')),   # bookings router e.g. /api/bookings/
-#     path('api/payments/', include('payments.urls')),
+
+#     # Users / Auth (namespaced)
+#     path('api/users/', include(('users.urls', 'users'), namespace='users')),
+
+#     # Bookings (all routes under /api/bookings/)
+#     path('api/bookings/', include(('bookings.urls', 'bookings'), namespace='bookings')),
+
+#     # Services (public + operator routes) under /api/services/
+#     path('api/services/', include(('services.urls', 'services'), namespace='services')),
+
+#     # Payments (already had its own base)
+#     path('api/payments/', include(('payments.urls', 'payments'), namespace='payments')),
 # ]
+
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 from django.contrib import admin
 from django.urls import path, include
@@ -32,20 +56,16 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    # Admin
+    path("admin/", admin.site.urls),
 
-    # Users / Auth (namespaced)
-    path('api/users/', include(('users.urls', 'users'), namespace='users')),
-
-    # Bookings (all routes under /api/bookings/)
-    path('api/bookings/', include(('bookings.urls', 'bookings'), namespace='bookings')),
-
-    # Services (public + operator routes) under /api/services/
-    path('api/services/', include(('services.urls', 'services'), namespace='services')),
-
-    # Payments (already had its own base)
-    path('api/payments/', include(('payments.urls', 'payments'), namespace='payments')),
+    # App URLs
+    path("api/services/", include("services.urls", namespace="services")),
+    path("api/bookings/", include("bookings.urls", namespace="bookings")),
+    path("api/payments/", include("payments.urls", namespace="payments")),
+    path("api/users/", include("users.urls", namespace="users")),
 ]
 
+# Only serve media files in development
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
